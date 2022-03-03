564e4f168fa87f4bcc5908fe23030b729edf1fc5
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-03-03 17:59:12 +0100

    Add array and popup

--- script/script.js
+++ script/script.js
@@ -1,96 +1,133 @@
-const openButn = document.querySelector('#menu-icon');
-const closeButn = document.querySelector('#closeBtn');
-const mobileNav = document.querySelector('.mob-wel');
-const mobileMen = document.querySelector('.mobil-men');
-const linkId = document.querySelector('#link');
-const links = document.querySelector('#links');
-const lin = document.querySelector('#linkss');
+// Mobile menu overlay
+const mobileNav = document.querySelector('#mobile-menu-navigation');
+const mobileNavMenu = document.querySelector('.overlay-content');
+const closeBtn = document.querySelector('.close-btn');
+const hamburger = document.querySelector('.menu-icon');
+const linkIds = Array.from(document.querySelectorAll('.page-link'));
 
-closeButn.addEventListener('click', () => {
-  mobileMen.style.display = 'none';
-  mobileMen.style.width = '0%';
-  mobileNav.style.display = 'block';
+hamburger.addEventListener('click', () => {
+  mobileNavMenu.style.display = 'block';
+  mobileNav.style.width = '100%';
 });
 
-openButn.addEventListener('click', () => {
-  mobileMen.style.height = '100vh';
-  mobileMen.style.width = '100%';
-  mobileMen.style.display = 'block';
-  closeButn.style.display = 'block';
-  mobileNav.style.display = 'none';
+closeBtn.addEventListener('click', () => {
+  mobileNavMenu.style.display = 'none';
+  mobileNav.style.width = '0%';
 });
 
-linkId.addEventListener('click', () => {
-  mobileMen.style.display = 'none';
-  mobileNav.style.display = 'block';
-});
+linkIds.forEach(
+  (linkId) => linkId.addEventListener('click', () => {
+    mobileNavMenu.style.display = 'none';
+    mobileNav.style.width = '0%';
+  }),
+);
 
-links.addEventListener('click', () => {
-  mobileMen.style.display = 'none';
-  mobileNav.style.display = 'block';
-});
+// End of mobile menu overlay
 
-lin.addEventListener('click', () => {
-  mobileMen.style.display = 'none';
-  mobileNav.style.display = 'block';
-});
+// Project popup window
+const docBody = document.querySelector('.project-popup-window');
+const projectButtons = Array.from(document.querySelectorAll('.works'));
+const wrapper = document.getElementById('wrapper');
+const body = document.getElementById('page-body');
+const projects = [
+  {
+    name: 'Keeping track of hundreds of components',
+    image: 'assets/images/Snapshoot-Portfolio.svg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://kimutai01.github.io/portfolio-mobile-view/',
+    source: 'https://github.com/Kimutai01',
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    image: 'assets/images/Snapshoot-Portfolio.svg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://kimutai01.github.io/portfolio-mobile-view/',
+    source: 'https://github.com/Kimutai01',
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    image: 'assets/images/Snapshoot-Portfolio.svg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://kimutai01.github.io/portfolio-mobile-view/',
+    source: 'https://github.com/Kimutai01',
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    image: 'assets/images/Snapshoot-Portfolio.svg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://kimutai01.github.io/portfolio-mobile-view/',
+    source: 'https://github.com/Kimutai01',
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    image: 'assets/images/Snapshoot-Portfolio.svg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://kimutai01.github.io/portfolio-mobile-view/',
+    source: 'https://github.com/Kimutai01',
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    image: 'assets/images/Snapshoot-Portfolio.svg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://kimutai01.github.io/portfolio-mobile-view/',
+    source: 'https://github.com/Kimutai01',
+  },
+];
+const projectCon = [];
 
-const projects = [{
-  title: 'Keeping track of hundreds of components',
-  image: 'assets/images/Snapshoot.svg',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  language1: 'Github',
-  language2: 'Codepen',
-  language3: 'JavaScript',
-  language4: 'Bootstrap',
-  language5: 'css',
-  language6: 'Terminal',
-  language7: 'Codekit',
-  language8: 'Ruby on Rails',
-}];
+const createProject = (projectArray) => {
+  for (let project = 0; project < projectArray.length; project += 1) {
+    projectCon.push(
+      `<div class="pop-up-overlay">
+         <span class="close" onclick="closeNav()"><img src="assets/images/Disabled.svg" alt=""></span>
+         <div class="pop-up-overlay-image">
+           <img class="pop-up-overlay-image" src="${projectArray[project].image}" alt="">
+         </div>
+         <div class="pop-up-overlay-content">
+           <div class="buttons-container">
+             <ul class="button-items">
+               <li><a href="${projectArray[project].live}">See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></i></a></li>
+               <li><a href="${projectArray[project].source}">See Source <i class="fa-brands fa-github"></i></a></li>
+             </ul>
+           </div>
+           <h2>${projectArray[project].name}</h2>
+           <ul class="lang-tags">
+             ${projectArray[project].technologies.map((tech) => (`<li class="tag">${tech}</li>`)).join('')}
+           </ul>
+           <p>${projectArray[project].description}</p>
+         </div>
+    </div>
+      `,
+    );
+  }
+};
+createProject(projects);
+for (let button = 0; button < projectButtons.length; button += 1) {
+  for (let content = 0; content < projectCon.length; content += 1) {
+    projectButtons[button].addEventListener('click', () => {
+      docBody.innerHTML = projectCon[content];
+      wrapper.classList.add('blur');
+      body.classList.add('color');
+    });
+  }
+}
 
-const [project] = projects;
-
-const button = document.querySelectorAll('.works-button');
-const popup = document.querySelector('.popup-window');
-
-popup.innerHTML = `<div class="container">
-<button class="close-button"><i class="fa fa-times"></i></button>
-<img src=${project.image} alt="project-img" class="popup-img">
-<div class="header-container">
-    <h2 class="container-title">${project.title}</h2>
-    <div class="desktop butt">
-<button class='seelive'>See Live<span><img src="assets/images/live.svg" alt="live"></span></button>
-</div>
-<div class='source butt'>
-<button class='seesource'>See Source<span><img src="assets/images/source.png" alt="source"></span></button>
-</div>
-</div>
-<ul class="skillsb">
-<li class="mobile">${project.language1}</li>
-<li class="mobile">${project.language2}</li>
-<li class="mobile">${project.language3}</li>
-<li class="desktop">${project.language4}</li>
-<li class="desktop">${project.language5}</li>
-<li class="desktop">${project.language6}</li>
-<li class="desktop">${project.language7}</li>
-<li class="desktop">${project.language8}</li>
-</ul>
-<p class="description">${project.description}</p>
-<div class="buttons non">
-<button>See Live<span><img src="assets/images/live.svg" alt="github"></span></button>
-<button>See Source<span><img src="assets/images/source.png" alt="github"></span></button>
-</div>
-</div>`;
-const close = document.querySelector('.close-button');
-close.addEventListener('click', () => {
-  popup.classList.replace('window', 'disappear');
-  document.body.classList.remove('permanent');
-});
-
-button.forEach((but) => {
-  but.addEventListener('click', () => {
-    document.body.classList.add('permanent');
-    popup.classList.replace('disappear', 'window');
-  });
-});
+// when triggered overwrite the HTML content and make it empty
+function closeNav() {
+  docBody.innerHTML = '';
+  wrapper.classList.remove('blur');
+  body.classList.remove('color');
+}
+closeNav();
